cb63ed8f9953bf73bf6059aa4e0f97b700f462dd
CumbersomeFellow <duthie93@gmail.com>
2026-09-12 11:18:26 +0100

    Feat: Added kennel availability service and some placeholder functions within

---
+++ BoardingKennelBookings/Components/Services/KennelAvailabilityService.cs
@@ -0,0 +1,28 @@
+﻿using BoardingKennelBookings.Components.Data;
+using BoardingKennelBookings.Components.Models;
+
+namespace BoardingKennelBookings.Components.Services
+{
+    public class KennelAvailabilityService
+    {
+        private readonly KennelContext _context;
+
+        public KennelAvailabilityService(KennelContext context) 
+        { 
+            _context = context;
+        }
+
+        public async Task<bool> CheckKennelAvailability(DateTime startDate, DateTime endDate, int kennelID)
+        {
+
+            return false;
+        }
+
+        //chech all kennels
+        //add is clean check later - shouldnt be hard to implement right!?
+        public async Task<List<Kennel>> GetAllKennelsAvailable(DateTime startDate, DateTime endDate)
+        {
+
+        }
+    }
+}
