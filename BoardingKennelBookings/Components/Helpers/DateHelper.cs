c9054b6a38c69c9c514f90ee694620ba5cb095f1
CumbersomeFellow <duthie93@gmail.com>
2026-09-13 15:43:25 +0100

    Feat: Added new date validation helper class. Implemented it to kennel checker.

---
+++ BoardingKennelBookings/Components/Helpers/DateHelper.cs
@@ -0,0 +1,10 @@
+﻿namespace BoardingKennelBookings.Components.Helpers
+{
+    public class DateHelper
+    {
+        public static bool IsValidDateRange(DateTime startDate, DateTime endDate)
+        {
+            return startDate < endDate;
+        }
+    }
+}
