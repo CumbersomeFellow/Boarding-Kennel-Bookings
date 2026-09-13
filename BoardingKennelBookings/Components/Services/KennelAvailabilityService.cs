90989b11abbe6f524fef8ab8b1f34642d7de226f
CumbersomeFellow <duthie93@gmail.com>
2026-09-13 15:22:36 +0100

    Fix: Now returning kennels if no bookings are found & fix on the create booking to use lists count instead of null

--- BoardingKennelBookings/Components/Services/KennelAvailabilityService.cs
+++ BoardingKennelBookings/Components/Services/KennelAvailabilityService.cs
@@ -62,6 +62,10 @@ namespace BoardingKennelBookings.Components.Services
                     }
                 }
             }
+            else
+            {
+                result = kennels;
+            }
 
             return result; 
         }
