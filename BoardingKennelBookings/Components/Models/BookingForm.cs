ef723369d9b92c72b3ecf4b1d200e10c3e738bdd
CumbersomeFellow <duthie93@gmail.com>
2026-09-16 08:00:07 +0100

    Feat: Adding some validation model side and output. Also started bookingdogkennel submission and select which dog goes to which kennel.

--- BoardingKennelBookings/Components/Models/BookingForm.cs
+++ BoardingKennelBookings/Components/Models/BookingForm.cs
@@ -1,15 +1,28 @@
-﻿namespace BoardingKennelBookings.Components.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BoardingKennelBookings.Components.Models
 {
     public class BookingForm
     {
-        public Guid OwnerID { get; set; }
-        public List<Guid> DogIDs { get; set; } = new List<Guid>();
-        public DateTime StartDate { get; set; }
-        public DateTime EndDate { get; set; }
+        [Required(ErrorMessage = "Please select an owner")]
+        public Guid? OwnerID { get; set; }
 
+        [MinLength(1, ErrorMessage = "Please select at least one dog.")]
+        public List<Guid> DogIDs { get; set; } = new List<Guid>();
+        
+        [Required]
+        public DateTime StartDate { get; set; }
+        
+        [Required]
+        public DateTime EndDate { get; set; }
+        
+        [Required]
         public bool AMPickup { get; set; }
+
+        [Required]
         public bool AMDropoff { get; set; }
-       
+
+        [MinLength(1, ErrorMessage = "Please select at least one kennel.")]
         public List<int> KennelIDs { get; set; } = new List<int>(); 
     }
 }
